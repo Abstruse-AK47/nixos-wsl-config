886be44ef03b6090a8965b0f9c21d9441d0fe362
AK <ayan344kapil@gmail.com>
2026-02-25 18:40:23 +0000

    added-nixvim-config

--- modules/home-manager/neovim/nixvim.nix
+++ modules/home-manager/neovim/nixvim.nix
@@ -36,6 +36,11 @@
 
       noice = {
         enable = true;
+        backgroundColor = "#000000";
+        fps = 60;
+        render = "minimal";
+        stages = "fade";
+        timeout = 2000;
         settings.presets = {
           bottom_search = true;
           command_pallet = true;
@@ -76,6 +81,8 @@
       { mode = "n"; key = "<leader>it"; action = ":lua ToggleTabLine()<CR>"; }
       { mode = "n"; key = "<leader>a"; action = ":lua require('harpoon.mark').add_file()<CR>"; }
       { mode = "n"; key = "<C-e>"; action = ":lua require('harpoon.ui').toggle_quick_menu()<CR>"; }
+      { mode = "n"; key = "<leader>d"; action = ":lua vim.diagnostic.open_float()<CR>"; options = { silent = true; desc = "Line Diagnostics"; };}
+      { mode = "n"; key = "<leader>D"; action = ":Telescope diagnostics bufnr=0<CR>"; options = { silent = true; desc = "Buffer Diagnostics (Telescope)";};}
     ];
   };
 }
