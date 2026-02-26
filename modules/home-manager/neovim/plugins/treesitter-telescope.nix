03009dea902ceec77f9a5d92f97c289824f15fb3
AK <ayan344kapil@gmail.com>
2026-02-26 12:21:21 +0000

    modified tmux and removed tree-sitter-cli not needed in nix

--- modules/home-manager/neovim/plugins/treesitter-telescope.nix
+++ modules/home-manager/neovim/plugins/treesitter-telescope.nix
@@ -1,4 +1,4 @@
-{ pkgs , ...}:
+{ _ , ...}:
 
 {
   programs.nixvim = {
@@ -31,8 +31,5 @@
         enableTelescope = true;
       };
     };
-    extraPackages = with pkgs; [
-      tree-sitter
-    ];
   };
 }
