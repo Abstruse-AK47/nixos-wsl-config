4b5fa0c605eab0a1059d76d1e90dd1db2c5bd791
AK <ayan344kapil@gmail.com>
2026-05-24 19:36:34 +0000

    added direnv

--- home.nix
+++ home.nix
@@ -8,7 +8,7 @@
 		./modules/home-manager/git/default.nix
 		./modules/home-manager/tmux/default.nix
 		./modules/home-manager/neovim/nixvim.nix
-    ./modules/home-manager/direnv/direnv.nix
+    ./modules/home-manager/direnv/default.nix
 		];
 	home = {
     username="nixos";
