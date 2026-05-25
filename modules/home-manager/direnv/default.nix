e307055eddbf65a6694cb37bcb04092d9dd2daaf
AK <ayan344kapil@gmail.com>
2026-05-25 16:31:19 +0000

    added direnv

--- modules/home-manager/direnv/default.nix
+++ modules/home-manager/direnv/default.nix
@@ -5,6 +5,13 @@
 programs.direnv = {
   enable = true;
   nix-direnv.enable = true;
+
+  # This provides the 'use devenv' capability to direnv globally
+  stdlib = ''
+    use_devenv() {
+      eval "$(devenv shell --print-bash-env)"
+    }
+  '';
 };
 
 }
